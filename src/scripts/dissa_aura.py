cd5ef10230f59669a623601596162baa457cb5f2
Richymaestro <xxxx@yyy.com>
2023-08-26 14:04:49 +0200

    testing tx

--- src/scripts/dissa_aura.py
+++ src/scripts/dissa_aura.py
@@ -1,24 +1,22 @@
-from roles_royce.roles_royce.protocols.eth import balancer, aura
-from roles_royce.roles_royce import send
-from scripts.abis import aura_rewards_contract_abi, balancer_pool_token_abi,balancer_vault_abi
+from roles_royce.protocols.eth import balancer, aura
+from roles_royce import send
+from roles_royce.protocols.base import AvatarAddress, Address
+from safe import Safe
+from abis import aura_rewards_contract_abi, balancer_pool_token_abi,balancer_vault_abi
+
+
 
 print('crap')
 
-def exit_aura_withdraw_and_unwrap(blockchain,percentage,safe_address,roles_mod,role,private_key,bpt_address,aura_rewards_contract_address,w3) -> None:
-    
-    aura_rewards_contract = w3.eth.contract(address=aura_rewards_contract_address,abi=aura_rewards_contract_abi)
+def exit_aura_withdraw_and_unwrap(w3, safe_address=AvatarAddress, aura_rewards_addr=Address, role=int, roles_mod=Address, private_key=None, blockchain=None):
+
+
+    aura_rewards_contract = w3.eth.contract(address=aura_rewards_addr,abi=aura_rewards_contract_abi)
     aura_rewards_amount = aura_rewards_contract.functions.balanceOf(safe_address).call()
+    print(aura_rewards_amount)
 
-    bpt_contract = w3.eth.contract(address=bpt_address, abi=balancer_pool_token_abi)
-    bpt_pool_id = bpt_contract.functions.getPoolId().call()
-    bpt_vault_address = bpt_contract.functions.getVault().call()
-
-    vault_contract = w3.eth.contract(address=bpt_vault_address, abi=balancer_vault_abi)
-    pool_id_tokens = vault_contract.functions.getPoolTokens(bpt_pool_id).call()[0]
-    assets = [token for tokens in pool_id_tokens for token in tokens]
-
-    withdraw_aura = aura.WithdrawAndUndwrapStakedBPT(reward_address=aura_rewards_contract_address, amount=int(aura_rewards_amount * 1))
-
-    send([withdraw_aura], role=role, private_key=private_key,
-                         roles_mod_address=roles_mod,
-                         blockchain=blockchain, web3=w3)
+    withdraw_aura = aura.WithdrawAndUndwrapStakedBPT(reward_address=aura_rewards_addr, amount=int(aura_rewards_amount * 1))
+    print(role)
+    tx = send([withdraw_aura], role=role, private_key=private_key, roles_mod_address=roles_mod, web3=w3)
+    print(tx.receipt)
+    print(aura_rewards_contract.functions.balanceOf(safe_address).call())
