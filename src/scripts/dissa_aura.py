0a57206b18427452df50819278e6fe256380865b
Richymaestro <xxxx@yyy.com>
2023-09-01 21:27:21 +0200

    working example

--- src/scripts/dissa_aura.py
+++ src/scripts/dissa_aura.py
@@ -1,22 +1,11 @@
 from roles_royce.protocols.eth import balancer, aura
-from roles_royce import send
+from roles_royce.roles import send, check, build
 from roles_royce.protocols.base import AvatarAddress, Address
 from safe import Safe
 from abis import aura_rewards_contract_abi, balancer_pool_token_abi,balancer_vault_abi
 
-
-
-print('crap')
-
 def exit_aura_withdraw_and_unwrap(w3, safe_address=AvatarAddress, aura_rewards_addr=Address, role=int, roles_mod=Address, private_key=None, blockchain=None):
-
-
     aura_rewards_contract = w3.eth.contract(address=aura_rewards_addr,abi=aura_rewards_contract_abi)
     aura_rewards_amount = aura_rewards_contract.functions.balanceOf(safe_address).call()
-    print(aura_rewards_amount)
-
     withdraw_aura = aura.WithdrawAndUndwrapStakedBPT(reward_address=aura_rewards_addr, amount=int(aura_rewards_amount * 1))
-    print(role)
     tx = send([withdraw_aura], role=role, private_key=private_key, roles_mod_address=roles_mod, web3=w3)
-    print(tx.receipt)
-    print(aura_rewards_contract.functions.balanceOf(safe_address).call())
--- src/scripts/main.py
+++ src/scripts/main.py
@@ -1,21 +1,22 @@
-from web3.exceptions import TransactionNotFound
 import time
-from dissa_aura import exit_aura_withdraw_and_unwrap
-
 import socket
 import os
 import subprocess
 import shlex
-from web3 import Web3, HTTPProvider
-from dotenv import load_dotenv
-
 import argparse
 import os
+import logging
+from dotenv import load_dotenv
+
+from web3 import Web3, HTTPProvider
+from web3.exceptions import TransactionNotFound
+
+from dissa_aura import exit_aura_withdraw_and_unwrap
+
 
 load_dotenv()
 
 def main():
-
     parser = argparse.ArgumentParser(description="Dummy testing script", epilog='This is the epilog',
                                  formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument("-p", "--percentage", type=int, default=100, help="percentage of liquidity to be removed")
@@ -24,11 +25,11 @@ def main():
     config = vars(args)
 
     ETH_FORK_NODE_URL = os.getenv("ETHEREUM_PW")
-    LOCAL_NODE_DEFAULT_BLOCK = 17565000
+    LOCAL_NODE_DEFAULT_BLOCK = 17612540
     cmd=f"anvil --accounts 15 -f {ETH_FORK_NODE_URL} --fork-block-number {LOCAL_NODE_DEFAULT_BLOCK} --port 8546"
     subprocess.Popen(shlex.split(cmd))
 
-    roles = 1
+    roles = 4
     roles_mod = "0x1cFB0CD7B1111bf2054615C7C491a15C4A3303cc"
     safe_address = "0x849D52316331967b6fF1198e5E32A0eB168D039d"
     private_key = "blabla"
@@ -36,16 +37,14 @@ def main():
 
     if blockchain == "fork":
         w3 = Web3(HTTPProvider(f"http://localhost:8546"))
-        aura_rewards_addr = "0x59D66C58E83A26d6a0E35114323f65c3945c89c1"
+        aura_rewards_addr = "0x6c3f6C327DE4aE51a2DfAaF3431b3c508ec8D3EB"
 
         tx = exit_aura_withdraw_and_unwrap(w3=w3, safe_address=safe_address, aura_rewards_addr=aura_rewards_addr,
                                        role=roles, roles_mod=roles_mod, private_key=private_key, blockchain=blockchain)
-        print('daar is em',tx)
-
-# if txn_receipt.status == 1:
-#     txns_message = '*Txn hash (Success):* <https://gnosisscan.io/tx/%s|%s>\n' % (executed_txn.hex(), executed_txn.hex())
-# else:
-#     txns_message = '*Txn hash (Fail):* <https://gnosisscan.io/tx/%s|%s>\n' % (executed_txn.hex(), executed_txn.hex())
+        if tx.status == 1:
+            txns_message = '*Txn hash (Success):* <https://gnosisscan.io/tx/%s|%s>\n' % (tx.hex(), tx.hex())
+        else:
+            txns_message = '*Txn hash (Fail):* <https://gnosisscan.io/tx/%s|%s>\n' % (tx.hex(), tx.hex())
 
 # print(txns_message)
 if __name__ == "__main__":
