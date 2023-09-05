dfc48571d371ce11216a177a6f8a5c6852867adf
Richymaestro <xxxx@yyy.com>
2023-09-05 21:52:21 +0200

    add ether send for gas

--- src/scripts/main.py
+++ src/scripts/main.py
@@ -23,6 +23,7 @@ def start_local_blockchain():
     subprocess.Popen(shlex.split(cmd))
 
 
+
 def main():
     parser = argparse.ArgumentParser(description="Dummy testing script", epilog='This is the epilog',
                                  formatter_class=argparse.ArgumentDefaultsHelpFormatter)
@@ -40,6 +41,7 @@ def main():
     roles_mod = "0x1cFB0CD7B1111bf2054615C7C491a15C4A3303cc"
     safe_address = "0x849D52316331967b6fF1198e5E32A0eB168D039d"
     private_key = os.getenv("PRIVATE_KEY")
+    EOA_address = "0xb11ea45e2d787323dFCF50cb52b4B3126b94810d"
     
     if not private_key:
         print("Private key is not set!")
@@ -49,6 +51,8 @@ def main():
 
     if blockchain == "fork":
         w3 = Web3(HTTPProvider(f"http://localhost:8546"))
+        w3.eth.send_transaction({"to": EOA_address, "value": Web3.to_wei(0.01, "ether")})
+        print(w3.eth.get_balance(EOA_address))
         aura_rewards_addr = "0x6c3f6C327DE4aE51a2DfAaF3431b3c508ec8D3EB"
 
         tx = exit_aura_withdraw_and_unwrap(w3=w3, safe_address=safe_address, aura_rewards_addr=aura_rewards_addr,
