d4c4becb4f44097fa3892984f8f10d665be9dbc5
Richymaestro <xxxx@yyy.com>
2023-09-07 23:06:07 +0200

    some testing

--- src/scripts/dissa_aura.py
+++ src/scripts/dissa_aura.py
@@ -2,33 +2,37 @@ from roles_royce.protocols.eth import balancer, aura
 from roles_royce.roles import send, check, build
 from roles_royce.protocols.base import AvatarAddress, Address
 from roles_royce.utils import tenderly_simulate, simulate_tx, tenderly_share_simulation
-
+from web3 import Web3
 from dotenv import load_dotenv
 import os
-
 from safe import Safe
-from abis import aura_rewards_contract_abi, balancer_pool_token_abi,balancer_vault_abi
-
+from abis import aura_rewards_contract_abi, balancer_pool_token_abi, balancer_vault_abi
 load_dotenv()
-
-def exit_1(simulate=bool, w3=None, safe_address=AvatarAddress, aura_rewards_addr=Address, role=int, roles_mod=Address, private_key=None, blockchain=None):
-    aura_rewards_contract = w3.eth.contract(address=aura_rewards_addr,abi=aura_rewards_contract_abi)
-    aura_rewards_amount = aura_rewards_contract.functions.balanceOf(safe_address).call()
-    withdraw_aura = aura.WithdrawAndUndwrapStakedBPT(reward_address=aura_rewards_addr, amount=int(aura_rewards_amount * 1))
-    EOA_account = "0xb11ea45e2d787323dFCF50cb52b4B3126b94810d"
-    if simulate:
-        withdraw_tx = build([withdraw_aura], role=role, account=EOA_account,
-                           roles_mod_address=roles_mod,
-                           web3=w3)
-        sim_data = simulate_tx(withdraw_tx, block=17000000, account_id=os.getenv("TENDERLY_ID"), project=os.getenv("TENDERLY_PROJECT"),
-                            api_token=os.getenv("TENDERLY_API"),
-                            sim_type='quick')
-        simulate_link = tenderly_share_simulation(account_id=os.getenv("TENDERLY_ID"), project=os.getenv("TENDERLY_PROJECT"), api_token=os.getenv("TENDERLY_API"),
-                                    simulation_id=sim_data['simulation']['id'])
-        return simulate_link
+def exit_1(simulate: bool, w3: Web3, avatar_safe_address: str, roles_mod_address: str, role: int, private_key: str,
+           args_dict: dict):
+  aura_rewards_address = args_dict['rewarder_address']
+  aura_rewards_contract = w3.eth.contract(address=aura_rewards_address, abi=aura_rewards_contract_abi)
+  aura_rewards_amount = aura_rewards_contract.functions.balanceOf(avatar_safe_address).call()
+  withdraw_aura = aura.WithdrawAndUndwrapStakedBPT(reward_address=aura_rewards_address,
+                                                   amount=int(aura_rewards_amount * 1))
+  EOA_account = "0xb11ea45e2d787323dFCF50cb52b4B3126b94810d"
+  if simulate:
+    withdraw_tx = build([withdraw_aura], role=role, account=EOA_account,
+                        roles_mod_address=roles_mod_address,
+                        web3=w3)
+    block = w3.eth.block_number
+    sim_data = simulate_tx(withdraw_tx, block=block, account_id=os.getenv("TENDERLY_ID"),
+                           project=os.getenv("TENDERLY_PROJECT"),
+                           api_token=os.getenv("TENDERLY_API"),
+                           sim_type='quick')
+    simulate_link = tenderly_share_simulation(account_id=os.getenv("TENDERLY_ID"),
+                                              project=os.getenv("TENDERLY_PROJECT"),
+                                              api_token=os.getenv("TENDERLY_API"),
+                                              simulation_id=sim_data['simulation']['id'])
+    return simulate_link
+  else:
+    tx = send([withdraw_aura], role=role, private_key=private_key, roles_mod_address=roles_mod_address, web3=w3)
+    if tx.status == 1:
+      return '*Txn hash (Success):* https://gnosisscan.io/tx/{}\n'.format(tx.transactionHash.hex())
     else:
-        tx = send([withdraw_aura], role=role, private_key=private_key, roles_mod_address=roles_mod, web3=w3)
-        if tx.status == 1:
-            return '*Txn hash (Success):* https://gnosisscan.io/tx/{}\n'.format(tx.transactionHash.hex())
-        else:
-            return '*Txn hash (Fail):* https://gnosisscan.io/tx/{}\n'.format(tx.transactionHash.hex())
+      return '*Txn hash (Fail):* https://gnosisscan.io/tx/{}\n'.format(tx.transactionHash.hex())
--- src/scripts/main.py
+++ src/scripts/main.py
@@ -7,35 +7,27 @@ import argparse
 import os
 import logging
 from dotenv import load_dotenv
-
 from web3 import Web3, HTTPProvider
 from web3.exceptions import TransactionNotFound
-
 from dissa_aura import exit_1
-
 load_dotenv()
-
 def start_local_blockchain():
     ETH_FORK_NODE_URL = os.getenv("ETHEREUM_PW")
     LOCAL_NODE_DEFAULT_BLOCK = 17612540
     cmd = f"anvil --accounts 15 -f {ETH_FORK_NODE_URL} --fork-block-number {LOCAL_NODE_DEFAULT_BLOCK} --port 8546"
     subprocess.Popen(shlex.split(cmd))
-
 def main():
     parser = argparse.ArgumentParser(description="Dummy testing script", epilog='This is the epilog',
                                  formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument("-p", "--percentage", type=int, default=100, help="percentage of liquidity to be removed")
     parser.add_argument("-e", "--execute", action='store_true', help="execute transaction")
-    parser.add_argument("--simulate", action='store_true', default=os.getenv("SIMULATE", "False").lower() == "true", 
+    parser.add_argument("--simulate", action='store_true', default=os.getenv("SIMULATE", "False").lower() == "true",
                         help="simulate transaction (default: False, or value of SIMULATE environment variable)")
     args = parser.parse_args()
     config = vars(args)
-
     start_local = os.getenv("START_LOCAL_BLOCKCHAIN", "").lower() == "yes"
-
     if start_local:
         start_local_blockchain()
-
     roles = 4
     roles_mod = "0x1cFB0CD7B1111bf2054615C7C491a15C4A3303cc"
     safe_address = "0x849D52316331967b6fF1198e5E32A0eB168D039d"
@@ -45,18 +37,14 @@ def main():
     if not private_key:
         print("Private key is not set!")
         return
-
     blockchain = "fork"
-
     if blockchain == "fork":
         w3 = Web3(HTTPProvider(f"http://localhost:8546"))
         w3.eth.send_transaction({"to": EOA_address, "value": Web3.to_wei(0.01, "ether")})
         print(w3.eth.get_balance(EOA_address))
         aura_rewards_addr = "0x6c3f6C327DE4aE51a2DfAaF3431b3c508ec8D3EB"
-
-        tx = exit_1(simulate=simulate, w3=w3, safe_address=safe_address, aura_rewards_addr=aura_rewards_addr,
-                                       role=roles, roles_mod=roles_mod, private_key=private_key, blockchain=blockchain)
+        tx = exit_1(simulate=simulate, w3=w3, avatar_safe_address=safe_address, roles_mod_address=roles_mod,
+                    role=roles, private_key=private_key, args_dict={"rewarder_address": aura_rewards_addr})
         print(tx)
- 
 if __name__ == "__main__":
     main()
