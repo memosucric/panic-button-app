33fd2ac22d0790f315ab469a91ec7e905944f513
Mariano Aguero <mariano.aguero@gmail.com>
2023-10-01 17:11:35 -0300

    feat: downgrade libraries and update next config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true
+  reactStrictMode: true,
+  images: {
+    domains: ['panic.karpatkey.com', 'localhost', 'panic.karpatkey.dev']
+  }
 }
 
 module.exports = nextConfig
