a1ca25ec78e92d0de097c5a4dc19ccd348ca6608
sgzerbo <santiago@karpatkey.com>
2023-10-05 17:06:18 -0300

    refactor: ens disassembling main

--- src/scripts/mainENSDAOEthereum.py
+++ src/scripts/mainENSDAOEthereum.py
@@ -1,125 +1,85 @@
-import time
-import socket
-import os
-import subprocess
-import shlex
 import argparse
 import os
-import logging
-
 from dotenv import load_dotenv
-from web3 import Web3, HTTPProvider
-from web3.exceptions import TransactionNotFound
-
-from roles_royce.utils import tenderly_simulate, simulate_tx, tenderly_share_simulation
+from web3 import Web3
 from roles_royce.toolshed.disassembling.Aura.disassembling_aura import AuraDisassembler
-from roles_royce.toolshed.disassembling.Balancer.disassembling_balancer import BalancerDisassembler
 
-from dissa_aura import exit_1
 load_dotenv()
 
-def start_local_blockchain():
-    ETH_FORK_NODE_URL = os.getenv("ETHEREUM_PW")
-    LOCAL_NODE_DEFAULT_BLOCK = 17612540
-    cmd = f"anvil --accounts 15 -f {ETH_FORK_NODE_URL} --fork-block-number {LOCAL_NODE_DEFAULT_BLOCK} --port 8546"
-    subprocess.Popen(shlex.split(cmd))
 
 def main():
-    parser = argparse.ArgumentParser(description="This is the real ENSDAO disassembling script", epilog='build by karpatkey',
-                                 formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    
-    parser.add_argument("-e", "--execute", action='store_true', help="execute transaction")
-    parser.add_argument("-sim", "--simulate", action='store_true', default=os.getenv("SIMULATE", "False").lower() == "true",
-                        help="simulate transaction (default: False, or value of SIMULATE environment variable)")
+  parser = argparse.ArgumentParser(description="This is the real ENS DAO disassembling script",
+                                   epilog='Build by karpatkey',
+                                   formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
-    parser.add_argument("-p", "--percentage", type=int, default=100, help="percentage of liquidity to be removed")
-    parser.add_argument("-s", "--positionId", type=str,help="The name of the position to execute the strategy for")
-    parser.add_argument("-s", "--exitStrategy", type=str,help="Exit strategy to execute")
-    parser.add_argument("-a", "--exitArguments", type=dict,help="Custom exit-arguments for each Exit strategy, i.e. max-slippage")
+  parser.add_argument("-e", "--execute", action='store_true', help="execute transaction")
+  parser.add_argument("-sim", "--simulate", action='store_true',
+                      default=os.getenv("SIMULATE", "False").lower() == "true",
+                      help="simulate transaction (default: False, or value of SIMULATE environment variable)")
 
-    args = parser.parse_args()
-    config = vars(args)
+  parser.add_argument("-p", "--percentage", type=int, default=100,
+                      help="Percentage of liquidity to be removed, defaults to 100.")
+  parser.add_argument("-", "--protocol", type=str, help="Protocol where the funds are deposited.")
+  parser.add_argument("-s", "--exit-strategy", type=str, help="Exit strategy to execute, e.g. exit_2_1, exit_3")
+  parser.add_argument("-a", "--exit-arguments", type=list[dict],
+                      help="List of dictionaries with the custom exit arguments for each position and exit strategy, " \
+                           "e.g. { 'bpt_address': '0xsOmEAddResS', 'max_slippage': 0.01}")
+  args = parser.parse_args()
 
-    start_local = os.getenv("START_LOCAL_BLOCKCHAIN", "").lower() == "yes"
-    if start_local:
-        start_local_blockchain()
-    
-    roles = os.getenv("ENSDAOETH_DISASSEMBLER_ROLE")
-    roles_mod = os.getenv("ENSDAOETH_ROLES_MOD")
-    safe_address = os.getenv("ENSDAOETH_AVATAR") 
-    private_key = os.getenv("PRIVATE_KEY")
-    EOA_address = os.getenv("ENSDAOETH_EOA")
-    
-    simulate = args.simulate
-    percentage = args.percentage
-    position_id = args.positionId
-    exit_strategy = args.exitStrategy
-    exit_arguments = args.exitArguments
-    
+  roles = os.getenv("ENSDAOETH_DISASSEMBLER_ROLE")
+  roles_mod = os.getenv("ENSDAOETH_ROLES_MOD")
+  safe_address = os.getenv("ENSDAOETH_AVATAR")
+  private_key = os.getenv("PRIVATE_KEY")
+  EOA_address = Web3.eth.account.from_key(private_key).address
 
-    if not private_key:
-        print("Private key is not set!")
-        return
+  simulate = args.simulate
+  percentage = args.percentage
+  exit_strategy = args.exitStrategy
+  exit_arguments = args.exitArguments
+  protocol = args.protocol
 
-    blockchain = "fork"
-    if blockchain == "fork":
-        w3 = Web3(HTTPProvider(f"http://localhost:8546"))
-        w3.eth.send_transaction({"to": EOA_address, "value": Web3.to_wei(0.01, "ether")})
-        print(w3.eth.get_balance(EOA_address))
-        if position_id == "Aura_11" or position_id == "Aura_219":
-            aura_disassembler = AuraDisassembler(w3=w3,
-                                                 avatar_safe_address=safe_address,
-                                                 roles_mod_address=roles_mod,
-                                                 role=roles,
-                                                 signer_address=EOA_address)
-            if exit_strategy == "Exit_1":
-                txn_transactable = aura_disassembler.exit_1(percentage=percentage,
-                                                            exit_arguments=[{"rewards_address": exit_arguments['rewarder_address']}])
-            elif exit_strategy == "Exit_2_1":
-                txn_transactable = aura_disassembler.exit_2_1(percentage=percentage,
-                                                            exit_arguments=[{"rewards_address": exit_arguments['rewarder_address'],
-                                                                "max_slippage": exit_arguments['max_slippage']}])
-            elif exit_strategy == "Exit_2_2":
-                txn_transactable = aura_disassembler.exit_2_2(percentage=percentage,
-                                                            exit_arguments=[{"rewards_address": exit_arguments['rewarder_address'],
-                                                                "token_out_address": exit_arguments['token_out_address'],
-                                                                "max_slippage": exit_arguments['max_slippage']}])
-            elif exit_strategy == "Exit_2_3":
-                txn_transactable = aura_disassembler.exit_2_3(percentage=percentage,
-                                                            exit_arguments=[{"rewards_address": exit_arguments['rewarder_address'],
-                                                                "amounts_out": exit_arguments['amounts_out'],
-                                                                "max_slippage": exit_arguments['max_slippage']}])
-            elif exit_strategy == "Exit_2_4":
-                txn_transactable = aura_disassembler.exit_2_4(percentage=percentage,
-                                                            exit_arguments=[{"rewards_address": exit_arguments['rewarder_address'], 
-                                                                "max_slippage": exit_arguments['max_slippage']}])
-        elif position_id == "Lido_15" or position_id == "Lido_149":
-            pass
-            #TODO: make the lido disassembler, to get this code working
-            # lido_disassembler = LidoDisassembler(w3=w3,
-            #                                      avatar_safe_address=safe_address,
-            #                                      roles_mod_address=roles_mod,
-            #                                      role=roles,
-            #                                      signer_address=EOA_address)
-            # if exit_strategy == "Exit_1":
-            #     txn_transactable = lido_disassembler.exit_1(percentage=percentage) 
-            # elif exit_strategy == "Exit_2":
-            #     txn_transactable = lido_disassembler.exit_2(percentage=percentage)   
-            # elif exit_strategy == "Exit_3":
-            #     txn_transactable = lido_disassembler.exit_3(percentage=percentage)   
-            # elif exit_strategy == "Exit_4":
-            #     txn_transactable = lido_disassembler.exit_4(percentage=percentage,
-            #                                                 exit_arguments=[{"max_slippage": exit_arguments['max_slippage']}])  
-            # elif exit_strategy == "Exit_5":
-            #     txn_transactable = lido_disassembler.exit_5(percentage=percentage,
-            #                                                 exit_arguments=[{"max_slippage": exit_arguments['max_slippage']}])             
-        if simulate:
-            exit_tx = aura_disassembler.simulate(txn_transactable)
-        else:
-            exit_tx = aura_disassembler.send(txn_transactable, private_key)
+  if not private_key:
+    raise Exception("Private key is not set!")
 
-        return exit_tx
+  w3 = Web3(Web3.HTTPProvider(os.getenv("RPC_ENDPOINT")))
+  if not w3.is_connected():
+    w3 = Web3(Web3.HTTPProvider(os.getenv("FALLBACK_RPC_ENDPOINT")))
+    if not w3.is_connected():
+      raise Exception("No connection to RPC endpoint")
 
-    
+  # TODO: sanity check with ETH balance
+
+  if protocol == "Aura":
+    disassembler = AuraDisassembler(w3=w3,
+                                    avatar_safe_address=safe_address,
+                                    roles_mod_address=roles_mod,
+                                    role=roles,
+                                    signer_address=EOA_address)
+  else:
+    raise Exception("Invalid protocol")
+
+  exit_strategy = getattr(disassembler, exit_strategy)
+
+  txn_transactable = exit_strategy(percentage=percentage, exit_arguments=exit_arguments)
+
+  try:
+    if simulate:
+      exit_tx = disassembler.simulate(txn_transactable)
+    else:
+      # TODO: add a sanity check with the 'check' function of the roles and the amount of ETH needed
+      exit_tx = disassembler.send(txn_transactable, private_key)
+    # TODO: build the dictionary to be returned
+    """
+    {"status" : 200,
+    "link": etherscan or tenderly link
+    "message": "Transaction executed successfully" or "Transaction reverted"
+    }
+    """
+    # The simulate function should also return the status: successful or reverted
+    return exit_tx
+  except Exception as e:
+    return {"status": 500, "message": f"Error: {e}"}
+
+
 if __name__ == "__main__":
-    main()
+  main()
