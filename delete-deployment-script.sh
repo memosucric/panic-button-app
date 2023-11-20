52531ab09b9e5cadcb27b4693496820077bdd0cb
dharmendra kariya <dharamendra.kariya@gmail.com>
2023-11-20 17:27:47 +0530

    ci: final workflow for pr review deployment

--- delete-deployment-script.sh
+++ delete-deployment-script.sh
@@ -1,15 +1,6 @@
 #!/bin/bash
 
-# GitHub repository and PR API endpoint
-# GITHUB_REPO="karpatkey/panic-button-app"
 PR_NUMBER=$1
-# GITHUB_API_URL="https://api.github.com/repos/${GITHUB_REPO}/commits/${GITHUB_SHA}/pulls"
-
-# DOCKER_TAG=$(echo $1 | cut -c1-7)
-
-# Extract PR number from the GitHub API response
-# PR_NUMBER=$(curl -s ${GITHUB_API_URL} | jq -r '.[0].number')
-
 
 # Apply the temporary Kubernetes deployment
 kubectl delete deployment -n panic-pr-review develop-panic-button-app-${PR_NUMBER}
