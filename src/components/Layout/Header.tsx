8ab04589ae888e68355bc40a952a9a199bf8655e
Mariano Aguero <mariano.aguero@gmail.com>
2024-01-23 10:57:28 -0300

    feat: fix username width

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -1,23 +1,22 @@
 import LogoKarpatkey from 'src/components/LogoKarpatkey'
 import BoxWrapperRow from 'src/components/Wrappers/BoxWrapperRow'
 import React from 'react'
-import {useUser} from '@auth0/nextjs-auth0/client'
-import {Avatar, Button} from '@mui/material'
-import {useRouter} from 'next/navigation'
+import { useUser } from '@auth0/nextjs-auth0/client'
+import { Avatar, Button } from '@mui/material'
+import { useRouter } from 'next/navigation'
 import CustomTypography from 'src/components/CustomTypography'
-import BoxWrapperColumn from 'src/components/Wrappers/BoxWrapperColumn'
 import useMediaQuery from '@mui/material/useMediaQuery'
 
 export const HEADER_HEIGHT = 100
 
 const NotLoggedComponent = () => {
-  const {push} = useRouter()
+  const { push } = useRouter()
 
   const onLogin = () => {
     push('/api/auth/login')
   }
   return (
-    <Button onClick={onLogin} sx={{gap: 2, height: '48px', padding: '6px 14px'}}>
+    <Button onClick={onLogin} sx={{ gap: 2, height: '48px', padding: '6px 14px' }}>
       Login
     </Button>
   )
@@ -26,12 +25,11 @@ const NotLoggedComponent = () => {
 interface LoggedComponentProps {
   name: string
   image: string
-  dao: string
 }
 
 const LoggedComponent = (props: LoggedComponentProps) => {
-  const {name, image} = props
-  const {push} = useRouter()
+  const { name, image } = props
+  const { push } = useRouter()
 
   const matches = useMediaQuery((theme: any) => theme.breakpoints.up('sm'))
 
@@ -41,20 +39,20 @@ const LoggedComponent = (props: LoggedComponentProps) => {
   return (
     <BoxWrapperRow>
       <BoxWrapperRow>
-        {matches && <Avatar alt={name} src={image}/>}
+        {matches && <Avatar alt={name} src={image} />}
         <CustomTypography
           ellipsis={true}
           sx={{
             gap: 2,
-            maxWidth: '140px',
+            maxWidth: 'min-content',
             padding: '6px 14px',
-            alignItems: 'center',
+            alignItems: 'center'
           }}
         >
           {name}
         </CustomTypography>
       </BoxWrapperRow>
-      <Button onClick={onLogout} sx={{gap: 2, height: '48px', padding: '6px 14px'}}>
+      <Button onClick={onLogout} sx={{ gap: 2, height: '48px', padding: '6px 14px' }}>
         Logout
       </Button>
     </BoxWrapperRow>
@@ -62,17 +60,14 @@ const LoggedComponent = (props: LoggedComponentProps) => {
 }
 
 const Header = () => {
-  const {user, isLoading} = useUser()
+  const { user, isLoading } = useUser()
 
-  const roles = user?.['http://localhost:3000/roles']
-    ? (user?.['http://localhost:3000/roles'] as unknown as string[])
-    : ['']
   const name = user?.name ?? ''
   const image = user?.picture ?? ''
 
   const loggedComponentProps = {
     name,
-    image,
+    image
   }
 
   return (
@@ -86,11 +81,11 @@ const Header = () => {
         height: HEADER_HEIGHT
       }}
     >
-      <LogoKarpatkey/>
+      <LogoKarpatkey />
       <BoxWrapperRow>
         {!isLoading ? (
           !user ? (
-            <NotLoggedComponent/>
+            <NotLoggedComponent />
           ) : (
             <LoggedComponent {...loggedComponentProps} />
           )
