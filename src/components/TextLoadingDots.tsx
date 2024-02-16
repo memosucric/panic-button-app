af5ec5326f29daf18a8cc2374f4e9b641a4344f8
Ismael Abreu <ismaelga@users.noreply.github.com>
2024-02-16 17:16:15 +0000

    fix: camel case styles

--- src/components/TextLoadingDots.tsx
+++ src/components/TextLoadingDots.tsx
@@ -2,10 +2,10 @@ import * as React from 'react'
 import { styled } from '@mui/system'
 
 const TypingAnimated = styled('span')({
-  'clip-path': 'inset(0 3ch 0 0)',
+  clipPath: 'inset(0 3ch 0 0)',
   '@keyframes typing': {
     to: {
-      'clip-path': 'inset(0 -1ch 0 0)'
+      clipPath: 'inset(0 -1ch 0 0)'
     }
   },
   animation: 'typing 2s steps(4) infinite'
